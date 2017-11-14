0c6b0ca136694f4c75f58b8ab89ff5f2c6a56183
Jess Brawner <jessnbillbrawner@gmail.com>
2017-11-14 13:13:36 -0600

    deployed through firebase

---
+++ public/javascripts/AppConstants.js
@@ -0,0 +1,10 @@
+"use strict";
+
+app.constant("FIREBASE_CONFIG", {
+    apiKey: "AIzaSyCXnmdTQZFCIs5pJp1UQfaYFlv9GIJ0iEo",
+    authDomain: "movie-groovy.firebaseapp.com",
+    databaseURL: "https://movie-groovy.firebaseio.com",
+    projectId: "movie-groovy",
+    storageBucket: "",
+    messagingSenderId: "887204138860"
+  });
