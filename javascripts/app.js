f47cfe60345a2a0c6a32ca64e9793ab3cce3e422
Jess Brawner <jessnbillbrawner@gmail.com>
2017-11-18 10:15:30 -0600

    added star rating plug in

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,6 +2,6 @@
 
 "use strict";
 
-const app = angular.module("MovieApp", ["ngAnimate", "ngRoute", "ngSanitize"]);
+const app = angular.module("MovieApp", ["ngAnimate", "ngRoute", "ngSanitize", "star-rating"]);
 
 console.log("test", app);
